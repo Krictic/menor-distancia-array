59d410732b56b91fdb6cc546a6843bcf1e9bdb52
MyriadColors <pedrotorresgamedesign@gmail.com>
2023-10-17 19:09:03 -0300

    Refatorização do programa e encapsulamento de certas operações em funções auxiliares.

--- Program.cs
+++ Program.cs
@@ -1,39 +1,66 @@
-﻿var arr2 = new List<int>() { 1, 2, 3 };
-var arr1 = new List<int>() { 10, 20, 30 };
-MenorDistancia(arr1, arr2);
-
-void MenorDistancia(List<int> array1, List<int> array2)
+﻿namespace menor_distancia_array
 {
-    while (true)
+    internal static class Program
     {
-        var maxArray1 = array1[0];
-
-        foreach (var i in array1)
+        public static void Main(string[] args)
         {
-            if (i > maxArray1)  
-                maxArray1 = i;
+            var arr2 = new List<int>() { 1, 2, 3 };
+            var arr1 = new List<int>() { 10, 20, 30 };
+            MenorDistancia(arr1, arr2);
         }
 
-        var minArray2 = 2147483647;
-        
-        foreach (var num in array2)
+        private static void MenorDistancia(List<int> array1, List<int> array2)
         {
-            if (num > maxArray1 && num < minArray2)
+            while (true)
             {
-                minArray2 = num;
+                var maxArray1 = ObterValorMaximo(array1);
+
+                var minArray2 = int.MaxValue;
+                minArray2 = ObterValorMinimo(array2, maxArray1, minArray2);
+
+                if (minArray2 != int.MaxValue)
+                {
+                    Console.WriteLine(minArray2 - maxArray1);
+                }
+                else
+                {
+                    TrocarArrays(ref array1, ref array2);
+                    continue;
+                }
+
+                break;
             }
         }
 
-        if (minArray2 != int.MaxValue)
+        private static int ObterValorMinimo(List<int> array2, int maxArray1, int minArray2)
         {
-            Console.WriteLine(minArray2 - maxArray1);
-        }
-        else
-        {
-            (array1, array2) = (array2, array1);
-            continue;
+            foreach (var num in array2)
+            {
+                if (num > maxArray1 && num < minArray2)
+                {
+                    minArray2 = num;
+                }
+            }
+
+            return minArray2;
         }
 
-        break;
+        private static int ObterValorMaximo(List<int> array)
+        {
+            var max = int.MinValue;
+            foreach (var num in array)
+            {
+                if (num > max)
+                {
+                    max = num;
+                }
+            }
+            return max;
+        }
+
+        private static void TrocarArrays(ref List<int> array1, ref List<int> array2)
+        {
+            (array1, array2) = (array2, array1);
+        }
     }
 }
